111974964de8f049ae0f05ca03041416e8b4d923
victor jose mahecha arango <vjmahechaa@gmail.com>
2022-05-05 20:07:44 +0200

    lives and score

--- app/src/main/java/de/buw/se4de/OnlineTest.java
+++ app/src/main/java/de/buw/se4de/OnlineTest.java
@@ -20,20 +20,28 @@ class OnlineTest extends JFrame implements ActionListener
 	
 	
 	
-	JLabel l, l_quiz;
+	JLabel l, l_quiz, l_numQuestion,l_headline, l_correctAnswer, l_lives;
 	JRadioButton jb[]=new JRadioButton[5];
 	JButton b1,b2;
 	ButtonGroup bg;
 	String current_quiz;
-	int count=0,current=1,x=1,y=1,now=0,current_correct=0;
+	int count=0,current=1,x=1,y=1,now=0,current_correct=0, lives = 3;
 	int m[]=new int[10];	
 	OnlineTest(String s)
 	{
 		super(s);
 		l=new JLabel();
 		l_quiz = new JLabel();
+		l_numQuestion = new JLabel();
+		l_headline=new JLabel();
+		l_correctAnswer = new JLabel();
+		l_lives = new JLabel();
 		add(l_quiz);
 		add(l);
+		add(l_numQuestion);
+		add(l_headline);
+		add(l_correctAnswer);
+		add(l_lives);
 		bg=new ButtonGroup();
 		for(int i=0;i<5;i++)
 		{
@@ -42,20 +50,28 @@ class OnlineTest extends JFrame implements ActionListener
 			bg.add(jb[i]);
 		}
 		b1=new JButton("Next");
-		b2=new JButton("Result");
+		b2=new JButton("End Game");
 		b1.addActionListener(this);
 		b2.addActionListener(this);
 		add(b1);add(b2);
-		set();
-		l.setBounds(30,10,650,90);
-		l_quiz.setBounds(450,300,100,30);
 		
+		set();
+		l_headline.setText("<html><span style='font-size:15px'>Quiz Challenge</html>");
+		l_lives.setText("<html> lives: "+ lives+"</html>");
+		l_headline.setBounds(30,30,200,20);
+		l_quiz.setBounds(30,60,400,30);
+		l_lives.setBounds(400,60,100,30);
+		l_numQuestion.setBounds(550,60,100,30);
+		l.setBounds(30,70,650,90);
+		l_correctAnswer.setBounds(500,320,100,30);
+		
+
 		for(int i=0,j=0;i<=120;i+=40,j++) {
-			jb[j].setBounds(50,120+i,450,30);
+			jb[j].setBounds(50,145+i,450,30);
 		}
 		
-		b1.setBounds(100,300,100,30);
-		b2.setBounds(270,300,100,30);
+		b1.setBounds(30,320,100,30);
+		b2.setBounds(200,320,100,30);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(null);
 		setLocation(250,100);
@@ -68,57 +84,42 @@ class OnlineTest extends JFrame implements ActionListener
 			
 		if(e.getSource()==b1)
 		{
-			if(check())
+			if(check()) {
 				count=count+1;
+			}else {
+				lives=lives-1;
+			}
+			
+			l_lives.setText("<html> lives: "+ lives+"</html>");
+				
 			current++;
 			set();	
-			if(current==limitQuestions)
+			if(current==limitQuestions || lives ==0)
 			{
 				b1.setEnabled(false);
-				b2.setText("Result");
+				b2.setText("End Game");
+				l_lives.setText("<html> lives: Game Over </html>");
 			}
 		}
 		
 		
 		
-		if(e.getActionCommand().equals("Result"))
+		if(e.getActionCommand().equals("End Game"))
 		{
-			//JButton bk=new JButton("Bookmark"+x);
-			//bk.setBounds(480,20+30*x,100,30);
-			//add(bk);
-			//bk.addActionListener(this);
-			//m[x]=current;
-			//x++;
-			//current++;
-			//set();	
 			if(current==limitQuestions)
-				b2.setText("Result");
+				b2.setText("End Game");
 			setVisible(false);
 			setVisible(true);
 		}
-		/*
-		for(int i=0,y=1;i<x;i++,y++)
-		{
-		if(e.getActionCommand().equals("Bookmark"+y))
-		{
-			if(check())
-				count=count+1;
-			now=current;
-			current=m[y];
-			set();
-			((JButton)e.getSource()).setEnabled(false);
-			current=now;
-		}
-		} */
+
 		 
 		
 		
-		if(e.getActionCommand().equals("Result"))
+		if(e.getActionCommand().equals("End Game"))
 		{
 			if(check())
 				count=count+1;
 			current++;
-			//System.out.println("correct ans="+count);
 			JOptionPane.showMessageDialog(this,"correct ans="+count);
 			System.exit(0);
 		}
@@ -131,7 +132,7 @@ class OnlineTest extends JFrame implements ActionListener
 		
 		ArrayList question_data  = new ArrayList(myQuestions.getQuestions().get(Integer.toString(current)));
 
-		if(current <= limitQuestions) {		
+		if(current <= limitQuestions && lives !=0) {		
 			
 		
 			l.setText("<html>"+(String) question_data .get(2) +"</html>");
@@ -150,14 +151,14 @@ class OnlineTest extends JFrame implements ActionListener
 				current_quiz = "Artificial Intelligence";  
 			}    
 			
-			l_quiz.setText("<html>"+current_quiz  +"</html>");  
-		
+			l_quiz.setText("<html> Topic: "+ current_quiz+"</html>");
+			l_numQuestion.setText("<html>"+"Question :"+current+ "/"+ limitQuestions  +"</html>");
+			l_correctAnswer.setText("<html> Correct Answers: "+ count+"</html>");
+			
+		   
 		}
 		
-		//l.setBounds(30,40,450,20);
-		for(int i=0,j=0;i<=120;i+=40,j++) {
-			jb[j].setBounds(50,90+i,450,30);
-		}
+		
 	}
 	boolean check()
 	{
